36ca8cd441afc7d0a6b876920709b2c8e73322ed
raihansyahyoga <raihansyahyoga@gmail.com>
2023-05-29 00:25:29 +0700

    connected to flask

--- src/api/index.js
+++ src/api/index.js
@@ -1,5 +1,6 @@
 const express = require('express');
 
+const request = require('request');
 const auth = require('./auth/auth.routes');
 const users = require('./users/users.routes');
 const posts = require('./posts/posts.routes');
@@ -12,6 +13,14 @@ router.get('/', (req, res) => {
   });
 });
 
+router.get('/home', (req, res) => {
+  request('http://127.0.0.1:5000/flask', (error, response, body) => {
+    console.error('error:', error); // Print the error
+    console.log('statusCode:', response && response.statusCode); // Print the response status code if a response was received
+    console.log('body:', body); // Print the data received
+    res.send(body); // Display the response on the website
+  });
+});
 router.use('/auth', auth);
 router.use('/users', users);
 router.use('/posts', posts);
