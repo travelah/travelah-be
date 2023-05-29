8442dc325ae74a80794a6cb48eefe32209c8efeb
raihansyahyoga <raihansyahyoga@gmail.com>
2023-05-29 15:27:27 +0700

    add status to error middleware

--- src/middleware/middleware.js
+++ src/middleware/middleware.js
@@ -10,6 +10,7 @@ function errorHandler(err, req, res, next) {
   const statusCode = res.statusCode !== 200 ? res.statusCode : 500;
   res.status(statusCode);
   res.json({
+    status: false,
     message: err.message,
     stack: process.env.NODE_ENV === 'production' ? '🥞' : err.stack,
   });
