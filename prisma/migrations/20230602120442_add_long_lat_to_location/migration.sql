7073a98edb919d978251162990e072018ee1e8f5
raihansyahyoga <raihansyahyoga@gmail.com>
2023-06-02 19:15:02 +0700

    fix capital on table name

--- prisma/migrations/20230602120442_add_long_lat_to_location/migration.sql
+++ prisma/migrations/20230602120442_add_long_lat_to_location/migration.sql
@@ -1,4 +1,4 @@
 -- AlterTable
-ALTER TABLE `post` ADD COLUMN `latitude` DECIMAL(65, 30) NULL,
+ALTER TABLE `Post` ADD COLUMN `latitude` DECIMAL(65, 30) NULL,
     ADD COLUMN `longitude` DECIMAL(65, 30) NULL,
     MODIFY `location` VARCHAR(191) NULL;
