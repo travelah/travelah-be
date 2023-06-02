06626cce61465ac1fb4a895dc8b4190b4c9a7301
raihansyahyoga <raihansyahyoga@gmail.com>
2023-06-02 22:45:48 +0700

    upload profile Photos

--- src/api/users/users.routes.js
+++ src/api/users/users.routes.js
@@ -1,9 +1,14 @@
 const express = require('express');
+const multer = require('multer');
 const { isAuthenticated } = require('../../middleware/middleware');
-const { findUserById } = require('./users.services');
+const {
+  findUserById,
+  updateProfile,
+  uploadProfilephoto,
+} = require('./users.services');
 
 const router = express.Router();
-
+const upload = multer({ dest: 'uploads/' });
 router.get('/profile', isAuthenticated, async (req, res, next) => {
   try {
     const { userId } = req.payload;
@@ -18,5 +23,56 @@ router.get('/profile', isAuthenticated, async (req, res, next) => {
     next(err);
   }
 });
+
+router.post(
+  '/profile',
+  isAuthenticated,
+  upload.single('photo'),
+  async (req, res, next) => {
+    try {
+      const { userId } = req.payload;
+      const user = await findUserById(userId);
+      let updatedProfile;
+      let updatedProfilePhoto;
+      delete user.password;
+      if (req.file != null) {
+        const photo = req.file;
+        if (!photo) {
+          res.status(400).json({ error: 'No photo provided' });
+          return;
+        }
+
+        // Process the photo and save it to Google Cloud Storage
+        const photoPath = photo.path;
+        const destinationPath = `profile-photos/${photo.originalname}`;
+
+        updatedProfilePhoto = await uploadProfilephoto(
+          photoPath,
+          destinationPath,
+          userId,
+          photo.originalname,
+        );
+        updatedProfile = await updateProfile(req.body, userId);
+
+        res.status(200).json({
+          data: updatedProfile,
+          // updatedProfile,
+          message: 'Success retrieve your profile',
+          status: true,
+        });
+      } else {
+        updatedProfile = await updateProfile(req.body, userId);
+        res.status(200).json({
+          data: updatedProfile,
+          // updatedProfile,
+          message: 'Success retrieve your profile',
+          status: true,
+        });
+      }
+    } catch (err) {
+      next(err);
+    }
+  },
+);
 
 module.exports = router;
--- src/api/users/users.services.js
+++ src/api/users/users.services.js
@@ -1,10 +1,63 @@
 const bcrypt = require('bcrypt');
+const { Storage } = require('@google-cloud/storage');
+const path = require('path');
+const { v4: uuidv4 } = require('uuid');
 const { db } = require('../../utils/db');
+const bucketServiceAccount = require('../../../keys/BucketCredential.json');
 
+// Specify the path to your credentials file
+// const keyFilename = path.resolve(
+//   __dirname,
+//   '../../../keys/bucketCredential.json',
+// );
+const publicUrl = 'https://storage.googleapis.com/$bucketName/$objectPath';
+
+const storage = new Storage({
+  credentials: bucketServiceAccount,
+});
+const bucketName = 'travelah-storage';
 function findUserByEmail(email) {
   return db.user.findUnique({
     where: {
       email,
+    },
+  });
+}
+
+async function uploadProfilephoto(
+  photoPath,
+  destinationPath,
+  userId,
+  photoOriginalName,
+) {
+  const bucket = storage.bucket(bucketName);
+
+  // Generate a unique file name using UUID and preserve the file extension
+  const uniqueFileName = `${uuidv4()}.jpg`; // Change the extension if the file is in a different format
+
+  const file = await bucket.upload(photoPath, {
+    destination: `${destinationPath}/${uniqueFileName}`,
+  });
+
+  const uploadedFile = file[0];
+  const filePath = `https://storage.googleapis.com/${bucketName}/${uploadedFile.name}`;
+
+  return db.user.update({
+    data: {
+      profilePicPath: filePath,
+      profilePicName: `${photoOriginalName}/${uniqueFileName}`,
+    },
+    where: {
+      id: userId,
+    },
+  });
+}
+
+async function updateProfile(user, userId) {
+  return db.user.update({
+    data: user,
+    where: {
+      id: userId,
     },
   });
 }
@@ -37,4 +90,6 @@ module.exports = {
   findUserById,
   createUserByEmailAndPassword,
   createUserOnlyByEmail,
+  updateProfile,
+  uploadProfilephoto,
 };
